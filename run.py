f6af9901b39840ae0b09e26c3b2aa88dd9402be3
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-01-16 10:58:39 -0600

    Run a single trial

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import yaml
-from psychopy import visual, event
+from psychopy import visual, event, core, sound
 
 
 class Experiment:
@@ -11,6 +11,9 @@ class Experiment:
         self.texts = yaml.load(open("texts.yaml"))
         self.win = visual.Window(units="pix")
 
+        self.fix = visual.TextStim(self.win, text="+", color="white")
+        self.noise = sound.Sound("noise.wav")
+
     def __call__(self):
         """Run the experiment.
 
@@ -18,12 +21,40 @@ class Experiment:
         >>> experiment()               # call the experiment to run it
         """
         self.show_instructions()
+        self.run_trial()
 
     def show_instructions(self):
         instructions = visual.TextStim(self.win, text=self.texts["instructions"], color="white")
         instructions.draw()
         self.win.flip()
         event.waitKeys()
+
+    def run_trial(self):
+        mov = visual.MovieStim(self.win, filename="vids/f_1_EC_L.mp4")
+
+        self.fix.draw()
+        self.win.flip()
+        self.noise.play()
+        core.wait(0.25)
+
+        key, rt = "", -1
+        timer = core.Clock()
+        while timer.getTime() < 4:
+            mov.draw()
+            self.win.flip()
+            response = event.getKeys(keyList=["right", "left"], timeStamped=timer)
+            if len(response) > 0:
+                key, rt = response[0]
+                break
+        else:
+            print("no response!")
+
+        trial_data = {
+            "key": key,
+            "rt": rt,
+        }
+
+        print(trial_data)
 
 
 if __name__ == "__main__":

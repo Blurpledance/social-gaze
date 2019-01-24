cb3cb585fae0d34f1f5244c7c8d13d4f1ff35a2f
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-01-24 12:33:12 -0600

    Fix logic to show break transition

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ class Experiment:
         self.baseline = yaml.load(open("baseline.yaml"))
         self.recording = yaml.load(open("recording.yaml"))
         self.getready = yaml.load(open("getready.yaml"))
-        
+
 
         self.fix = visual.TextStim(self.win, text="+", color="white", height=150)
         self.noise = sound.Sound("stimuli/sounds/noise.wav")
@@ -41,41 +41,41 @@ class Experiment:
         >>> experiment()                        # call the experiment to run it
         """
         self.show_baseline()
-        
+
         self.show_recording()
-        
+
         self.show_getready()
-        
+
         self.show_instructions()
 
+        cur_block = 1
         for _, trial in self.trials.iterrows():
-            trial_data = self.run_trial(trial)
-            self.write_trial_data(trial_data)
-            
-        cur_block = 1  # starts at block 1
-        for _, trial in self.trials.iterrows():
-            if cur_block != trial["block_ix"]:
+            is_block_transition = (cur_block != trial["block_ix"])
+            if is_block_transition:
                 self.show_break_screen()
                 cur_block = trial["block_ix"]
-                
+
+            trial_data = self.run_trial(trial)
+            self.write_trial_data(trial_data)
+
     def show_baseline(self):
         baseline = visual.TextStim(self.win, text=self.baseline["baseline"], color="white")
         baseline.draw()
         self.win.flip()
         event.waitKeys()
-    
+
     def show_recording(self):
         recording = visual.TextStim(self.win, text=self.recording["recording"], color="white")
         recording.draw()
         self.win.flip()
         core.wait(300)
-        
+
     def show_getready(self):
         getready = visual.TextStim(self.win, text=self.getready["getready"], color="white")
         getready.draw()
         self.win.flip()
         event.waitKeys()
-    
+
     def show_break_screen(self):
         break_screen = visual.TextStim(self.win, text=self.breaktext["break_screen"], color="white")
         break_screen.draw()
@@ -87,7 +87,7 @@ class Experiment:
         instructions.draw()
         self.win.flip()
         event.waitKeys()
-    
+
     def run_trial(self, trial):
         mov = visual.MovieStim3(self.win, filename=trial["filename"])
 

2cfb8956029de1e50db05bf8fd162679209810a5
Pierce Edmiston <pierce.edmiston@gmail.com>
2019-02-05 15:08:21 -0600

    Add run.bat

---
+++ run.bat
@@ -0,0 +1,3 @@
+:: contents of experiment.bat
+:: Activate the root Anaconda python, then activate the psychopy environment, then run the experiment.
+%userprofile%\AppData\Local\Continuum\anaconda2\Scripts\activate & activate psychopy & python run.py

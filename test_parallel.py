e1c0ca61b18b5b0d02e976e6f8d9fb6c6455b49e
Blurpledance <44209381+Blurpledance@users.noreply.github.com>
2019-02-15 14:35:48 -0600

    trouble shoot

--- test_parallel.py
+++ test_parallel.py
@@ -1,3 +1,5 @@
 from psychopy import parallel
-parallel.setPortAddress(0XD020)
-parallel.setData(0)
+#parallel.setPortAddress(0XD020)
+port = parallel.ParallelPort(address=0XD020)
+print port.PORT
+port.setData(0)
